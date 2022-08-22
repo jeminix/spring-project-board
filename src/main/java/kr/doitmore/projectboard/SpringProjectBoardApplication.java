32530a9f56b66a3af5b13549c3bc409ae1c001d0
jeminix <jeminix04@gmail.com>
2022-08-22 21:22:52 +0900

    #6 - 스프링 부트 프로젝트 시작
    * Sprint Web
    * Actuator
    * Dev tools
    * Lombok

---
+++ src/main/java/kr/doitmore/projectboard/SpringProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package kr.doitmore.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProjectBoardApplication.class, args);
+    }
+
+}
