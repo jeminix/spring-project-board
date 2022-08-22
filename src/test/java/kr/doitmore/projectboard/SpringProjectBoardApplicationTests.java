daf114dc67e4cd08a8ffccfff8389f32b7ee76d8
jeminix <jeminix04@gmail.com>
2022-08-22 21:34:03 +0900

    Revert "#6-boot: Auto stash before rebase of "origin/feature/#6-boot""
    This reverts commit bd0626947070e16e6b61c7f267c84196fb67031d.

---
+++ src/test/java/kr/doitmore/projectboard/SpringProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.doitmore.projectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
